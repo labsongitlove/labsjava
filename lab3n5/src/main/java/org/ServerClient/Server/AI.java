a86c251a4c5e60d1e3987b5b111455dc4ce5b6d0
br1znasty <br1znasty@users.noreply.github.com>
2025-04-16 22:13:01 +0700

    Rebased to Gradle, testing Client-Server code and main Game.

---
+++ lab3n5/src/main/java/org/ServerClient/Server/AI.java
@@ -0,0 +1,25 @@
+package org.ServerClient.Server;
+
+
+import org.Game.Game;
+import org.Game.Player;
+
+public class AI {
+    int _type;
+
+    public AI(){
+        _type = 0;
+    }
+    public AI(int type){
+        _type = type;
+    }
+    public int DoBet(Player player, Game game) {
+        if (_type == 0){
+            return 50;
+        }
+        if (_type == 1){
+            return game.GetBetNow();
+        }
+        return 0;
+    }
+}
---
+++ lab3n5/src/main/java/org/ServerClient/Server/MessagesHandlerServer.java
@@ -0,0 +1,44 @@
+package org.ServerClient.Server;
+
+import org.ServerClient.Message;
+import org.Game.*;
+
+import java.util.ArrayList;
+
+public class MessagesHandlerServer {
+    private Game _game;
+
+    public MessagesHandlerServer(Game game){
+        _game = game;
+    }
+
+    public void UpdateGame(Game game){
+
+    }
+    /*public Message GetMessage(){
+        return new Message(0, 0, 0, _game.GetTable(), _game.GetPlayers());
+    }*/
+    public void Parsing(Message message, Player player){
+        if (message.GetType() == 1){
+            if (_game.GetStatus() == 1){
+                _game.DoBet(player, message.GetValue());
+            }
+        }
+    }
+    public Message MakeSafelyMessage(Player playerTo, int type, int value, String text){
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(playerTo);
+        for (Player player : _game.GetPlayers()){
+            if (player != playerTo){
+                players.add(player.MakeSafelyCopy());
+            }
+        }
+        return new Message(type, value, text, _game.GetTable(), players);
+    }
+    public Message MakeMessage(int type, int value, String text){
+        return new Message(type, value, text, _game.GetTable(), _game.GetPlayers());
+    }
+    public String GetNameInMessage(Message message){
+        return message.GetText();
+    }
+}
